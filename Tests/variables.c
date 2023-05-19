34776858cbf3093cef4b2e471486d712c6604209
Moustik06 <starlinator06@live.fr>
2023-05-19 11:45:14 +0200

    test restore

--- Tests/variables.c
+++ Tests/variables.c
@@ -1,13 +1,8 @@
+/* TEST VARIABLES MINIC */
+
 int a;
-
-extern int printd( int i );
-
-int test(int z,int w,int y) {
-  printd(z);
-}
-
-int main(int z) {
-  int a,toto,x,y,t;
-  toto = x*y - z /t *6;
-  return 3;
+int main() {
+  int a, b;
+  a = 0;
+  b = 1;
 }
